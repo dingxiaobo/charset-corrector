b8242d7b1ffab7b2ac8001b20d8c71f5f13efe79
sx-9300 <sx-dingxb@dtdream.com>
2017-07-11 10:37:52 +0800

    Initial commit

---
+++ src/main/java/cn/dxbtech/CharsetCorrectorApplication.java
@@ -0,0 +1,12 @@
+package cn.dxbtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharsetCorrectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharsetCorrectorApplication.class, args);
+	}
+}
